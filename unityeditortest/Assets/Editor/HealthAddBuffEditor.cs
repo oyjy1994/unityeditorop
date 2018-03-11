0af43321723883b7ad2b4d16667883a8cf92134b
oyjy1 <ouyangjianyu@163.com>
2018-03-11 17:07:56 +0800

    ScriptableObject and CustomEditor Test

---
+++ unityeditortest/Assets/Editor/HealthAddBuffEditor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(HealthAddBuff))]
+public class HealthAddBuffEditor : Editor 
+{
+    SerializedProperty valueProperty;
+    void OnEnable() 
+    {
+        valueProperty = serializedObject.FindProperty("value");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        HealthDelBuff buff = target as HealthDelBuff;
+
+        GUILayout.BeginVertical();
+
+        EditorGUILayout.PropertyField(valueProperty, new GUIContent("加血量"));
+
+        GUILayout.EndVertical();
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
---
+++ unityeditortest/Assets/Script/Health.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    private int m_health;
+
+    public void DeltaHealth(int delta)
+    {
+        m_health += delta;
+    }
+
+    public override string ToString()
+    {
+        return string.Format("Health value = {0}", m_health);
+    }
+}
