ff09fbfae1e5486012bfdd3b6ed6e9cccb3de220
oyjy1 <ouyangjianyu@163.com>
2018-03-18 19:16:12 +0800

    delete

--- unityeditortest/Assets/Script/BaseBuff.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-//改动会立即实施到所有地方
-abstract public class BaseBuff : ScriptableObject
-{
-    abstract public void ApplyBuff(GameObject obj);
-}
