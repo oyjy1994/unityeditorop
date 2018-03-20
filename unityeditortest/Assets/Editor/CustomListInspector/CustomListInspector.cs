19af5e522d0e7977289d3fda80e007edbf896da1
oyjy1 <ouyangjianyu@163.com>
2018-03-20 21:33:45 +0800

    editor list

---
+++ unityeditortest/Assets/Editor/CustomListInspector/CustomListInspector.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditorInternal;
+
+[CanEditMultipleObjects]
+[CustomEditor(typeof(CustomList))]
+public class CustomListInspector : Editor
+{
+    ReorderableList orderList;
+
+    void OnEnable() 
+    {
+        if (null == target)
+            return;
+
+        orderList = new ReorderableList(serializedObject, serializedObject.FindProperty("m_List"), true, true, true, true);
+        orderList.drawHeaderCallback = rect => 
+        {
+            EditorGUI.LabelField(new Rect(rect.x, rect.y, 60, rect.height), "名称");
+            EditorGUI.LabelField(new Rect(rect.width - 20, rect.y, 60, rect.height), "年龄");
+        };
+        orderList.drawElementCallback = (rect, index, isActive, isFocused) =>
+        {
+            var property = orderList.serializedProperty.GetArrayElementAtIndex(index);
+            var nameproperty = property.FindPropertyRelative("name");
+            var ageproperty = property.FindPropertyRelative("age");
+
+            EditorGUI.PropertyField(new Rect(rect.x, rect.y + 2, 60, EditorGUIUtility.singleLineHeight), nameproperty, GUIContent.none);
+            EditorGUI.PropertyField(new Rect(rect.width - 10, rect.y + 2, 40, EditorGUIUtility.singleLineHeight), ageproperty, GUIContent.none);
+        };
+        orderList.onAddCallback = list => 
+        {
+            int index = list.serializedProperty.arraySize;
+            list.serializedProperty.arraySize++;
+
+            var property = list.serializedProperty.GetArrayElementAtIndex(index);
+            property.FindPropertyRelative("name").stringValue = "New Name";
+            property.FindPropertyRelative("age").intValue = 0;
+
+            serializedObject.ApplyModifiedProperties();
+        };
+        orderList.onRemoveCallback = list =>
+        {
+            ReorderableList.defaultBehaviours.DoRemoveButton(list);
+
+            serializedObject.ApplyModifiedProperties();
+        };
+    }
+
+    //void 
+
+    public override void OnInspectorGUI()
+    {
+        //base.OnInspectorGUI();
+        serializedObject.ApplyModifiedProperties();
+        serializedObject.Update();
+
+        orderList.DoLayoutList();
+        serializedObject.ApplyModifiedProperties();
+    }
+}
---
+++ unityeditortest/Assets/Script/CustomList/CustomList.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomList : MonoBehaviour {
+    [System.Serializable]
+    public struct PersonalInfo
+    {
+        public string name;
+        public int age;
+    }
+    public List<PersonalInfo> m_List = new List<PersonalInfo>();
+    //{
+    //    new PersonalInfo(){ name = "Tom", age = 14 },
+    //    new PersonalInfo(){ name = "Bob", age = 17 },
+    //};
+}
